9646ae54f2ecfe16d8db52d3fbb07eeb8a6f3e89
aviadatlas <aviad.lianne@gmail.com>
2017-08-09 13:24:17 +0300

    Added DTW for finding which word is emphasized

---
+++ dtw.py
@@ -0,0 +1,47 @@
+from fastdtw import fastdtw
+
+from python_speech_features import mfcc
+from python_speech_features import logfbank
+import scipy.io.wavfile as wav
+from scipy.spatial.distance import euclidean
+from os import listdir
+
+from operator import itemgetter
+
+frames = 50#20
+first_frame = 30
+mfccs = 20 #upto 26!
+test_frac = 0.2
+
+input_file_name = './DTW single file/see%the%bombers fly up.wav'
+# Extract MFCCs from input wav file
+print(input_file_name)
+(rate, sig) = wav.read(input_file_name)
+mfcc_feat = mfcc(sig,rate)
+curr = logfbank(sig,rate)
+input_file_data = curr[first_frame:(first_frame+frames),0:mfccs]/20
+
+# Extract MFCCs from each permutation of 1 emphasized word
+file_names = []
+distances = []
+for file_name in listdir('./DTW single file'):
+    print(file_name)
+    file_names.append(file_name)
+    (rate, sig) = wav.read("./DTW single file/" + file_name)
+    mfcc_feat = mfcc(sig, rate)
+    curr = logfbank(sig, rate)
+    current_file_data = curr[first_frame:(first_frame + frames), 0:mfccs] / 20
+    distance, path = fastdtw(input_file_data, current_file_data, dist=euclidean)
+    distances.append(distance)
+
+print(file_names)
+print(distances)
+min_distance_index = min(enumerate(distances), key=itemgetter(1))[0]
+print(min_distance_index)
+
+s = file_names[min_distance_index]
+c = '%'
+start_end_indexes = [pos for pos, char in enumerate(s) if char == c]
+print(start_end_indexes)
+answer = file_names[min_distance_index][start_end_indexes[0]+1:start_end_indexes[1]]
+print(answer)
